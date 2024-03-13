19e17edfd0aad18969fed5a2a0932b3b9757c442
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-13 22:57:35 +0330

    add api

--- app/page.js
+++ app/page.js
@@ -3,9 +3,13 @@ import MyHeader from './components/header/MyHeader'
 import Albums from './components/albums/Albums'
 
 export default async function Home() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+  const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=50', {
+    headers: {
+      "x-api-key": "live_3gfLi49P7U65L6Xcc0YL0B2KQK12JVsgE69dUnmV71mEOdNjDCCBHkxL3x95chVt"
+    }
+  })
   const data = await res.json()
-  console.log(data)
+
   return (
     <main>
       <MyHeader />
--- app/recent/page.jsx
+++ app/recent/page.jsx
@@ -1,15 +1,19 @@
 
 
 async function RecentPage() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+    const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=50', {
+        headers: {
+            "x-api-key": "live_3gfLi49P7U65L6Xcc0YL0B2KQK12JVsgE69dUnmV71mEOdNjDCCBHkxL3x95chVt"
+        }
+    })
     const data = await res.json()
     return (
         <>
-        <div className="w-full fixed bg-black opacity-5"></div>
-        <h1 className="p-10 text-xl font-semibold text-white fixed">Recent</h1>
-            <div className="flex flex-wrap w-full gap-1 justify-center">
+            <div className="w-full fixed bg-black opacity-5"></div>
+            <h1 className="p-10 text-xl font-semibold text-white fixed">Recent</h1>
+            <div className="grid grid-cols-3 w-full gap-1">
                 {data.map(item => {
-                    return <img className="w-40" src={item.url} alt="" />
+                    return <div className="w-full aspect-square bg-center bg-cover" style={{ backgroundImage: `url(${item.url})` }}></div>
                 })}
             </div>
         </>
