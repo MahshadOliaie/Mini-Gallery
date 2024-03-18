18744da92a48a3abc5d592b30e9ba1d3ebb3947a
MahshadOliaie <mahshadoliaie@gmail.com>
2024-03-18 20:15:23 +0330

    add style

--- app/recent/[id]/page.jsx
+++ app/recent/[id]/page.jsx
@@ -1,12 +1,16 @@
 
 
-async function OneImage({params}){
+async function OneImage({ params }) {
+    console.log(params)
     const res = await fetch(`https://api.thecatapi.com/v1/images/${params.id}`)
     const data = await res.json()
-    return(
-    <>
-    <img src={data.url} alt="" />
-    </>
+    return (
+        <>
+            <div className="w-full h-screen flex items-center justify-center">
+                <img src={data.url} className="w-full max-h-screen" />
+            </div>
+
+        </>
     )
 }
 
